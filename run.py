768f17eafb4e5e3a44709b31d311e2ab98f821e8
Alan Diaz <alandiazquijada@ciencias.unam.mx>
2024-10-02 00:27:03 -0600

    Se implemento la entrada por ticket de la aplicacion.

--- run.py
+++ run.py
@@ -4,14 +4,12 @@ import os
 # Crear instancia de la aplicación Flask
 app = create_app()
 
-# Eliminar o comentar esta línea en producción.
-# print(type(app))  # Para verificar que la app está creada correctamente
 
 if __name__ == "__main__":
     # Configurar el host y puerto desde variables de entorno o usar valores predeterminados
-    host = os.environ.get('FLASK_RUN_HOST', '127.0.0.1')  # El host predeterminado es localhost
-    port = int(os.environ.get('FLASK_RUN_PORT', 5000))  # Puerto predeterminado 5000
-    debug_mode = os.environ.get('FLASK_DEBUG', 'True') == 'True'  # Activar depuración por defecto
+    host = os.environ.get('FLASK_RUN_HOST', '127.0.0.1')  
+    port = int(os.environ.get('FLASK_RUN_PORT', 5000))  
+    debug_mode = os.environ.get('FLASK_DEBUG', 'True') == 'True' 
 
     # Ejecutar la aplicación
     app.run(host=host, port=port, debug=debug_mode)
